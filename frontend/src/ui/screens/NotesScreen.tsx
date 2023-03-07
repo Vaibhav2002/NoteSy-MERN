3f1c48136c698f832d7130f9dda98a390c53a7ef
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-07 20:24:18 +0530

    Added Fab and dialog opening from it

--- frontend/src/ui/screens/NotesScreen.tsx
+++ frontend/src/ui/screens/NotesScreen.tsx
@@ -3,18 +3,27 @@ import React, {useEffect, useState} from "react"
 import Note from "../../data/models/Note";
 import NoteItem from "../components/noteItem/NoteItem";
 import Grid from '@mui/material/Unstable_Grid2';
+import {Box, Fab} from "@mui/material";
+import AddEditNoteModal from "../components/AddEditNoteModal/AddEditNoteModal";
+import {Add} from "@mui/icons-material";
+import styles from "./NoteScreen.module.css"
 
 const NotesScreen = () => {
-    const [notes, setNotes] = useState<Note[]>([]);
+    const [notes, setNotes] = useState<Note[]>([])
+    const [isModalOpen, setModalOpen] = useState(false)
+
+    async function fetchNotes() {
+        try {
+            const notes = await getAllNotes()
+            setNotes(notes)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     useEffect(() => {
         async function loadNotes() {
-            try {
-                const notes = await getAllNotes()
-                setNotes(notes)
-            } catch (error) {
-                console.log(error)
-            }
+            await fetchNotes()
         }
 
         loadNotes()
@@ -32,7 +41,29 @@ const NotesScreen = () => {
 
     return (
         <>
-            {notesGrid}
+            <Box className = {styles.noteScreen}>
+                <Box p={2}>{notesGrid}</Box>
+                {isModalOpen && <AddEditNoteModal
+                    onNoteSave={note => {
+                        alert(JSON.stringify(note))
+                        setModalOpen(false)
+                        setNotes([...notes, note]);
+                    }
+                    }
+                    onDismiss={() => {
+                        setModalOpen(false)
+                    }}
+                />
+                }
+                <Fab
+                    className={styles.addNoteFab}
+                    aria-label="add"
+                    color="primary"
+                    onClick={() => setModalOpen(true)}
+                >
+                    <Add/>
+                </Fab>
+            </Box>
         </>
     );
 };
