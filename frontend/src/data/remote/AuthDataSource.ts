2a38e382d781dc7e9a44d541ac92125479df828a
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-12 20:31:30 +0530

    designed AuthDataSource

---
+++ frontend/src/data/remote/AuthDataSource.ts
@@ -0,0 +1,24 @@
+import LoginCredentials from "./requests/LoginCredentials";
+import {apiCall} from "./ApiCall";
+import User from "../models/User";
+import RegisterCredentials from "./requests/RegisterCredentials";
+
+const loginUser = async (credentials: LoginCredentials) => {
+    const user = await apiCall("/auth/login", {
+        method: "POST",
+        headers: {'Content-Type': "application/json"},
+        body: JSON.stringify(credentials)
+    })
+    return user as User
+}
+
+const registerUser = async (credentials: RegisterCredentials) => {
+    const user = await apiCall("/auth/register", {
+        method: "POST",
+        headers: {'Content-Type': "application/json"},
+        body: JSON.stringify(credentials)
+    })
+    return user as User
+}
+
+export {loginUser, registerUser}
---
+++ frontend/src/data/remote/requests/RegisterCredentials.ts
@@ -0,0 +1,6 @@
+export default interface RegisterCredentials {
+
+    username:string
+    email: string
+    password: string
+}
