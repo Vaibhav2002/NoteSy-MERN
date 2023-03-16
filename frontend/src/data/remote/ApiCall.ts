6c536df7f32add317c3427ceedfff538a0083980
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-16 18:41:04 +0530

    ignore

--- frontend/src/data/remote/ApiCall.ts
+++ frontend/src/data/remote/ApiCall.ts
@@ -1,7 +1,7 @@
 
 const baseUrl = process.env.REACT_APP_BACKEND_URL
 export const apiCall = async (input:RequestInfo, init?:RequestInit) => {
-    const response = await fetch(`${baseUrl}${input}`, init)
+    const response = await fetch(`https://notesapp-backend.up.railway.app/api${input}`, init)
     if(!response.ok) {
         const error = await response.json()
         throw Error(error.error)
