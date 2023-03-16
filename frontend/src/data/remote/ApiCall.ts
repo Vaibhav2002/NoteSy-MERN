1b6bcea15da4a603062c6576f08d517b8c0908fd
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-16 19:31:36 +0530

    frontend env removed

--- frontend/src/data/remote/ApiCall.ts
+++ frontend/src/data/remote/ApiCall.ts
@@ -1,7 +1,5 @@
-
-const baseUrl = process.env.REACT_APP_BACKEND_URL
 export const apiCall = async (input:RequestInfo, init?:RequestInit) => {
-    const response = await fetch(`https://notesapp-backend.up.railway.app/api${input}`, init)
+    const response = await fetch(`/api${input}`, init)
     if(!response.ok) {
         const error = await response.json()
         throw Error(error.error)
