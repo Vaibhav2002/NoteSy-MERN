c19bf21434f61b338d9d62a2dd877284c90314a6
Vaibhav2002 <vaibhav.jaiswal3011@gmail.com>
2023-03-16 18:27:24 +0530

    frontend Env added

--- frontend/src/data/remote/ApiCall.ts
+++ frontend/src/data/remote/ApiCall.ts
@@ -1,5 +1,5 @@
 
-const baseUrl = process.env["BACKEND_URL"]
+const baseUrl = process.env.REACT_APP_BACKEND_URL
 export const apiCall = async (input:RequestInfo, init?:RequestInit) => {
     const response = await fetch(`${baseUrl}${input}`, init)
     if(!response.ok) {
